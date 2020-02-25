f3fb952669e5900a815ec2b7062a89313701c8dd
Alessandro Canicatti <ales.canicatti@gmail.com>
2020-02-25 22:44:37 +0100

    validator has a test project

---
+++ src/validator.test/Useless.cs
@@ -0,0 +1,14 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace validator.test
+{
+    public class Useless
+    {
+        [Fact]
+        private void pass()
+        {
+            true.Should().Be(true);
+        }
+    }
+}
